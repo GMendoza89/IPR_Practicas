ed12b269a80fc98c5e1bd8ff12b924455974c7bb
Gustavo David Mendoza Pinto <gustavo.mendoza.dp@gmail.com>
2021-12-13 19:02:15 -0600

    Se incorporan datos para la practica 03

---
+++ IPR_Practica_03.X/main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: gustavomendoza
+ *
+ * Created on December 13, 2021, 1:37 PM
+ */
+
+
+#include <xc.h>
+
+#define _XTAL_FREQ 20000000
+void main(void) {
+    __delay_ms(1);
+    return;
+}
