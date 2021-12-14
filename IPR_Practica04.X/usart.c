2026f037517d7ad681215aaff13f115f883e48f2
Gustavo David Mendoza Pinto <gustavo.mendoza.dp@gmail.com>
2021-12-13 19:02:55 -0600

    Se incoporan datos de la practica 04

---
+++ IPR_Practica04.X/usart.c
@@ -0,0 +1,15 @@
+/*
+ * File:   usart.c
+ * Author: gustavomendoza
+ *
+ * Created on December 13, 2021, 6:58 PM
+ */
+
+
+#include <xc.h>
+#include "usart.h"
+#include "configurationBits.h"
+
+void main(void) {
+    return;
+}
